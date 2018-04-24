e3a77b3d0edd78f640253b8360b026bdc7ac409f
Bronty Thai <dung.q.thai@student.uts.edu.au>
2018-04-24 16:44:38 +1000

    Initialisation set up

--- assignment/prg_1.c
+++ assignment/prg_1.c
@@ -19,10 +19,40 @@ void *runnerB(void *param); // Thread B function
 void *runnerC(void *param); // Thread C funtion
 void initData();
 
-int main(itn argc, char *argv[])
+int main(int argc, char *argv[])
 {
+    /* TBU: Probably error checking */
+    // if(argc!=2){
+	//     fprintf(stderr,"usage: a.out <integer value>\n");
+	//     return -1;
+    // }
+    // if(atoi(argv[1])<0){
+	//     fprintf(stderr,"%d must be >=0\n",atoi(argv[1]));
+	//     return -1;
+    // }
 
+    initData(); //Initialise threads and semaphores
+
+    //pthread_attr_init(&attr); //TODO: Might be uneccessary
+
+    // TODO: Get data from data.txt
+
+    // TODO: Maybe the last param is what should be passed in?
+    /* Create threads */
+    pthread_create(&tidA, &attr, runnerA, param); // Create thread A
+    pthread_create(&tidB, &attr, runnerB, param); // Create thread B
+    pthread_create(&tidC, &attr, runnerC, param); // Create thread C
+
+    sem_post(&semA); // Unlock semaphore A --This will allow thread A to run first
+
+    /* Wait for thread to exit */
+    pthread_join(tidA, NULL);
+    pthread_join(tidB, NULL);
+    pthread_join(tidC, NULL);
+
+    // TODO: Probably do something with the timer here
 }
+
 
 void initData()
 {
