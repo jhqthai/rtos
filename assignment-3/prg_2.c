698e2ed60063d0f82565bff7214816798ffd06fd
Bronty Thai <dung.q.thai@student.uts.edu.au>
2018-05-21 01:24:20 +1000

    prg_2 initial commit

---
+++ assignment-3/prg_2.c
@@ -0,0 +1,32 @@
+// Deadlock detection
+#include <stdio.h>
+
+#define DATA_FILE "Topic2_Prg_ 2.txt" 
+#define OUT_FILE "output_topic2.txt"
+
+int main(int argc, char *argv[])
+{
+    FILE *proc_fp = fopen(DATA_FILE, "r");
+    
+
+
+}
+// Read from file
+
+
+// Detect deadlock
+
+
+// Output process IDs sequence
+
+
+// Output process ID where deadlock occured if true
+
+
+// Write to file "output_topic2.txt"
+
+// Send user SIGUSR1? after program completed
+// -- oh lol its just a line "Writing to output_topic2.txt has finished"
+// maybe not lol
+
+// Generate gantt-chart for the lols
